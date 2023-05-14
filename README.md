4a99782bcafe361ed8aad299686622cbd3d442dc
sergenast <nastia0709@mail.ru>
2023-05-14 19:19:31 +0300

    homework week20

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # week20
+
+1. установить Node.js и проверить, все ли норм установилось, с помощью команд node -v и npm -v в терминале
+
+2. ввести команду npm -v 
+
+3. код, написанный в finally, сработает в любом случае, вне зависимости от того, произойдет ошибка или нет
+
+4. try {
+    let user = undefined;
+    console.log(`Привет, ${user.name}`);
+} catch {
+    console.log('Имя пользователя не заполнено');
+}
+
+5. с помощью throw new Error ('Некорректный формат данных')
+
+6. npm install или npm i
+
+7. try {
+    let number = 'ыыы'
+    console.log(parseInt(number));
+
+    if (number !== Number) {
+        throw new Error('Некорректные данные.Введите число')
+    }
+
+} catch {
+    console.log('Некорректные данные')
+}
+
+8. 
---
+++ index.js
@@ -0,0 +1,62 @@
+const select = document.querySelector('.search__select')
+const input = document.querySelector('.search__input')
+const button = document.querySelector('.search__button')
+const result = document.querySelector('.result')
+
+function getResult() {
+    try {
+        for (i = 1; i < 11; i++) {
+            if (select.value == "Людей" && input.value == i) {
+                fetch(`https://swapi.dev/api/people/ + ${i}`)
+                    .then(response => response.json())
+                    .then(character => {
+                        result.innerText = character.name
+                    })
+            }
+        }
+
+        if (select.value == "Людей" &&
+            i > 10) {
+            throw new Error("Введите, пожалуйста, значение, меньше или равное 10")
+        }
+    } catch (e) {
+        result.innerText = e.message
+    }
+    try {
+        for (i = 1; i < 11; i++) {
+            if (select.value == "Планеты" && input.value == i) {
+                fetch(`https://swapi.dev/api/planets/ + ${i}`)
+                    .then(response => response.json())
+                    .then(planet => {
+                        result.innerText = planet.name
+                    })
+            }
+        }
+
+        if (select.value == "Планеты" && i > 10) {
+            throw new Error("Введите, пожалуйста, значение, меньше или равное 10")
+        }
+    } catch (error) {
+        result.innerText = error.message
+    }
+
+    try {
+        for (i = 1; i < 7; i++) {
+            if (select.value == "Фильмы" && input.value == i) {
+                fetch(`https://swapi.dev/api/films/ + ${i}`)
+                    .then(response => response.json())
+                    .then(film => {
+                        result.innerText = film.title
+                    })
+            }
+        }
+
+        if (select.value == "Фильмы" && i > 6) {
+            throw new Error("Введите, пожалуйста, значение, меньше или равное 7")
+        }
+    } catch (err) {
+        result.innerText = err.message
+    }
+}
+
+button.addEventListener('click', getResult)
